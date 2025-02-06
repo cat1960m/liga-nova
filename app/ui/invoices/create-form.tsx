b808cef680aa0b1b0bad392a11c7cf3e82952219
Kateryna Dobrovolska <k.dobrovolska@sellerlogic.com>
2025-02-06 18:34:28 +0200

    chapter 15-16 make small correction

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -12,12 +12,12 @@ import { Button } from "@/app/ui/button";
 import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
 
-export default function Form1({ customers }: { customers: CustomerField[] }) {
+export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (
-    <Form action={formAction}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -142,6 +142,6 @@ export default function Form1({ customers }: { customers: CustomerField[] }) {
         </Link>
         <Button type="submit">Create Invoice</Button>
       </div>
-    </Form>
+    </form>
   );
 }
